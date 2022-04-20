86dafae02ade1c6100e1ac79abd88037d75965b1
Mateusz Arak <mateusz.arak@gmail.com>
2022-04-20 10:31:59 +0200

    choose

--- Gittie.cs
+++ Gittie.cs
@@ -1,5 +1,8 @@
 introduce("Hell nooo,  I'm Gittie!");
 joke("Knock, knock");
 add(int a, int b);
+<<<<<<< Updated upstream
 shout(args, sync);
 ;ah;kjghakhg;h;ahgha;khg
+=======
+>>>>>>> Stashed changes
